195c5dc1b5a322ed710b9c9f19ccb44a4211fd92
Joshua Kennedy <allamericanjbert@gmail.com>
2013-03-25 05:49:32 -0500

    Initial Commit

---
+++ TreeViewDirectoryStructure/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TreeViewDirectoryStructure
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.directoryTreeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(233, 25);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 20);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(12, 25);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(215, 20);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(12, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(244, 13);
+            this.instructionLabel.TabIndex = 2;
+            this.instructionLabel.Text = "Enter the full path you would like to view as a tree!";
+            // 
+            // directoryTreeView
+            // 
+            this.directoryTreeView.Location = new System.Drawing.Point(12, 51);
+            this.directoryTreeView.Name = "directoryTreeView";
+            this.directoryTreeView.Size = new System.Drawing.Size(296, 268);
+            this.directoryTreeView.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 331);
+            this.Controls.Add(this.directoryTreeView);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.generateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Directory Structure";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TreeView directoryTreeView;
+    }
+}
+
---
+++ TreeViewDirectoryStructure/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TreeViewDirectoryStructure
+{
+    public partial class MainForm : Form
+    {
+        string substr;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void PopulateTreeView(string directory, TreeNode parentNode)
+        {
+            string[] directoryArray = Directory.GetDirectories(directory);
+
+            try
+            {
+                if (directoryArray.Length != 0)
+                {
+                    foreach (string dir in directoryArray)
+                    {
+                        substr = Path.GetFileNameWithoutExtension(dir);
+
+                        TreeNode myNode = new TreeNode(substr);
+
+                        parentNode.Nodes.Add(myNode);
+
+                        PopulateTreeView(dir, myNode);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                parentNode.Nodes.Add("Access Denied");
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            generateButton.Enabled = false;
+            pathTextBox.Enabled = false;
+
+            if (Directory.Exists(pathTextBox.Text))
+            {
+                directoryTreeView.Nodes.Clear();
+
+                directoryTreeView.Nodes.Add(pathTextBox.Text);
+
+                PopulateTreeView(pathTextBox.Text, directoryTreeView.Nodes[0]);
+            }
+            else
+            {
+                generateButton.Enabled = true;
+                pathTextBox.Enabled = true;
+
+                MessageBox.Show("Whoops! The path you entered does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                pathTextBox.Focus();
+            }
+
+            generateButton.Enabled = true;
+            pathTextBox.Enabled = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            pathTextBox.Text = Environment.CurrentDirectory;
+        }
+    }
+}
